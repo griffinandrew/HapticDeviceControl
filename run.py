3bcafbd152f85f1b7797ae043a1a5f32554d603a
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-22 18:17:01 -0400

    Update run.py
    adding both proper threading and multiprocessing commands

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import subprocess
 import multiprocessing 
 from multiprocessing import Pool
 import os 
-
+import threading 
 
 
 haptic = ("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")
@@ -23,12 +23,29 @@ def run_os():
     #os.system("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe &")
 
 def run_mult_proc():
-        p1 = multiprocessing.Process(target="//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py")
-        p2 = multiprocessing.Process(target="C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")    
+        p1 = multiprocessing.Process(target=exec1, args=())
+        p2 = multiprocessing.Process(target=exec2, args=())    
 
         p1.start()
         p2.start()
-
+        
+        
+ def exec1():
+    os.system("//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py")
+    
+ def exec2():
+    os.system("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")
+    
+def run_thread():
+    t1 = threading.Thread(target=exe1, args=()) 
+    t2 = threading.Thread(target=exe2, args=()) 
+    t1.start()
+    t2.start() 
+    
 
 if __name__ == '__main__':
-    run_mult_proc()
+    run_thread()
+    #run_mult_proc()
+
+    
+    
