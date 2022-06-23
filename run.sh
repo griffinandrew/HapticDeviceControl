995908961eb6cf6a10e530521bdb0d4e76fb533c
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-23 14:53:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 python -m pip install time
 python -m pip install pyfirmata
 
-python3 -c "import time, pyrfirmata, code; code.interact(local=locals())"
+python3 -c "import time, pyfirmata, code; code.interact(local=locals())"
 
 mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/file_to_arduino.py &
 mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/pickApplesGLUT.exe && fg
