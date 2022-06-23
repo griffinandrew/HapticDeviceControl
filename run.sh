0123a89c8066da5bb739e7ce584f654d02605aa9
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-23 14:39:28 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 -c "import time, pyrfirmata, code; code.interact(local=locals())"
+
+mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/file_to_arduino.py &
+mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/pickApplesGLUT.exe && fg
