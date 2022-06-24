057eb3710f35190cb551047844b65f8d84541197
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-24 16:46:15 -0400

    Update file2ardiuno.py

--- file2ardiuno.py
+++ file2ardiuno.py
@@ -1,22 +1,18 @@
+#!/user/bin/env python
 import time
 import pyfirmata
-from pyfirmata import Arduino, util, STRING_DATA
+from pyfirmata import util, STRING_DATA
 
-import subprocess
-
-board = pyfirmata.Arduino("COM3")
-
+board = pyfirmata.Arduino("COM7")
+1
 def read_file(): 
     #open the file and write to arduino
     f = open("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\pickApples\\pickApplesGLUT\\positions1.txt", "r", encoding ='utf-8') 
 
-    if(f): #if file open
-        print("reading")
+    if(f):
         k =  f.read()
         board.send_sysex(STRING_DATA, util.str_to_two_byte_iter(k)) #sends the str
         print(k)
-        time.sleep(0.1) #sends item every .1 seconds to file for read 
-
 def main():
     while(1):
         read_file()
