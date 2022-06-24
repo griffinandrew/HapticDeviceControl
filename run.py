1c16a19708fc40f66162566d5b58514b0158f8ec
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-24 16:46:43 -0400

    Update run.py

--- run.py
+++ run.py
@@ -12,14 +12,14 @@ import threading
 import file2arduino
  
 
-haptic = ("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")
+haptic = "C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe"
 
-f2p= ("//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py")
+f2p= "//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py"
 
 
 def run():
     #p1 = subprocess.Popen(["C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe"])
-    p2 = subprocess.Popen("//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py & C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")
+    p2 = subprocess.call(['python.exe','//ad/eng/users/g/h/gheyrich/Desktop/file_to_arduino.py', 'C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe'])
 
    # subprocess.run("python3 fi.py & python3 script2.py", shell=True)
 
@@ -49,14 +49,27 @@ def run_thread():
     t1.start()
     #t2.start() 
 
+def exec3():
+    file2arduino.main()
 
 def run_module():
-    t1 = threading.Thread(target=)
-    file2arduino.main()
-    exec2()
+    t1 = threading.Thread(target=exec2, args= ())
+    t2 = threading.Thread(target=exec3, args=())
+    t1.start()
+    t2.start()
+
+    #p2 = multiprocessing.Process(target=exec2, args=()) 
+    #p2.start()  
+
+    #os.system("C:\\OpenHaptics\\Developer\\3.5.0\\Quickhaptics\\examples\\Debug\\pickApplesGLUT.exe")
+
+
+    #file2arduino.main()
+    #exec2()
     
 
 if __name__ == '__main__':
     #run_thread()
     #run_mult_proc()
-    run_module()
+    #run_module()
+    run()
