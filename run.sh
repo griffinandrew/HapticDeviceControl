e24f8d94209426e84e91547d9d68492cf4f9ca64
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2022-06-24 20:04:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,11 +8,16 @@ python3 -c "import time, pyfirmata, code; code.interact(local=locals())" // ng
 #!/bin/python
 import time
 import pyfirmata
-mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/file_to_arduino.py &
-
-
-
-
-
+/mnt/c/OpenHaptics/Developer/3.5.0/Quickhaptics/examples/Debug/file_to_arduino.py &
 
 #lol wait i think that I can just create an executable of the python file and then just have them both run concurrently from the script
+
+
+#the python exec is failing to connect to com port and also load other modules within the envi 
+
+
+
+
+#https://stackoverflow.com/questions/32093559/exe-file-created-by-pyinstaller-not-find-self-defined-modules-while-running?answertab=trending#tab-top
+#
+#basically the internet is saying need to specify paths of the included modules
